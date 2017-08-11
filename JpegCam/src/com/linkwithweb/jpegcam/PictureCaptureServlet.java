9a9c4c43dc2e4f7ca3b399f74c63c504052cabb6
ramana-007 <ramanamurthy.mca@gmail.com>
2017-08-11 22:24:45 +0530

    ok

--- JpegCam/src/com/linkwithweb/jpegcam/PictureCaptureServlet.java
+++ JpegCam/src/com/linkwithweb/jpegcam/PictureCaptureServlet.java
@@ -101,7 +101,7 @@ public class PictureCaptureServlet extends HttpServlet {
 			//IOUtils.copy(request.getInputStream(), response.getOutputStream());  
 			
 		/*	bytes = output.toByteArray(); 
-		 *   
+		 *     
 			String encodedString = Base64.getEncoder().encodeToString(bytes);	
 			System.out.println("Base64image::::"+encodedString);
 			fileOutputStream.close();
