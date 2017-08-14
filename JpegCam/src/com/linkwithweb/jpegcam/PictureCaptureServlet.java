a0378ef8b4220cfc4ff3bcda1f5bb3b8c30023d3
ramana-007 <ramanamurthy.mca@gmail.com>
2017-08-14 14:26:03 +0530

    ok

--- JpegCam/src/com/linkwithweb/jpegcam/PictureCaptureServlet.java
+++ JpegCam/src/com/linkwithweb/jpegcam/PictureCaptureServlet.java
@@ -96,8 +96,6 @@ public class PictureCaptureServlet extends HttpServlet {
 			 * To make sure each url is differeent and not cached added time to tit
 			 */      
 			
-			       
-			
 			//IOUtils.copy(request.getInputStream(), response.getOutputStream());  
 			 
 		/*	bytes = output.toByteArray();   
