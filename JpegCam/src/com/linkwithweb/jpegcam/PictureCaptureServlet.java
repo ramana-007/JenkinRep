f133d5dc6f91d338a4c421f3f1daf6968f148884
ramana-007 <ramanamurthy.mca@gmail.com>
2017-09-06 21:43:29 +0530

    updated

--- JpegCam/src/com/linkwithweb/jpegcam/PictureCaptureServlet.java
+++ JpegCam/src/com/linkwithweb/jpegcam/PictureCaptureServlet.java
@@ -26,7 +26,7 @@ import javax.servlet.http.HttpServletResponse;
      
 /**
  * Ashwin Kumar
- * Servlet implementation class PictureCaptureServlet 
+ * Servlet implementation class PictureCaptureServlet
  */ 
 public class PictureCaptureServlet extends HttpServlet {   
 	private static final long serialVersionUID = 1L;    
